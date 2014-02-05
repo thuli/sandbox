ac136fb5f612ff50dc55c41c7c8be29a1fea7cf1
Nate Thuli <nthuli10@gmail.com>
2014-02-05 11:35:41 -0500

    added skeleton

---
+++ sandbox.js
@@ -0,0 +1,31 @@
+//
+//  sandbox.js
+//    Example webserver with node, express, and jade
+//
+
+var express = require('express');
+var http = require('http');
+var fs = require('fs');
+
+var app = express();
+console.log(' launching webserver...');
+
+//----------------------------------------------------------
+
+http.createServer(app).listen(8000);
+
+app.set('views', __dirname+'/views');
+app.set('view engine', 'jade');
+app.use(express.static(__dirname+'/public');
+app.use(express.logger('dev'));
+app.use(app.router);
+app.use(function(req, res) {
+  res.status(404);
+  res.render('404', {'title': 'Page Not Found'});
+});
+
+//----------------------------------------------------------
+
+app.get('/', function(req, res) {
+  res.render('index');
+});
